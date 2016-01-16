a3e2a41a5431a87fb27a1887da123b035d333546
Sriram Panyam <sri.panyam@gmail.com>
2016-01-15 18:39:16 -0800

    Cleanup

--- Sources/WebSocket/WSMessage.swift
+++ Sources/WebSocket/WSMessage.swift
@@ -380,7 +380,6 @@ public class WSMessageReader
      */
     private func processNewFrame(frame: WSFrame, callback : CompletionCallback?)
     {
-        Log.debug("==================================================================")
         Log.debug("Processing New Frame: \(self.currentFrame)")
         self.currentFrame = frame
         if frame.reserved1Set || frame.reserved2Set || frame.reserved3Set
